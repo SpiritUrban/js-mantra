dfb626d1aff137bae61e1772900b1ccc4d8f207b
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-07-02 11:02:43 +0200

    refactoring and interfaces and types contacts

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,30 +1,62 @@
 "use client";
+
+import React from "react";
 import Table from "react-bootstrap/Table";
+import { members, Member, Contact } from "@/data/members";
+
+interface MemberRowsProps {
+  members: Member[];
+}
+
+interface ContactItemsProps {
+  contacts: Contact[];
+}
+
+const ContactItems: React.FC<ContactItemsProps> = ({ contacts }) =>
+  contacts.map((contact, contactIndex) => (
+    <div key={contactIndex}>
+      <a href={contact.link} target="_blank" rel="noopener noreferrer">
+        {contact.type}
+      </a>
+    </div>
+  ));
+
+const MemberRows: React.FC<MemberRowsProps> = ({ members }) => (
+  <>
+    {members.map((member, index) => (
+      <tr key={index + "table-member"}>
+        <td>{index + 1}</td>
+        <td>{member.name}</td>
+        <td>
+          <ContactItems contacts={member.contacts} />
+        </td>
+        <td>{member.status}</td>
+        <td>
+          <a
+            href={member.github.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {member.github.title || "GitHub"}
+          </a>
+        </td>
+        <td>
+          {member.portfolioLink && (
+            <a
+              href={member.portfolioLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Portfolio
+            </a>
+          )}
+        </td>
+      </tr>
+    ))}
+  </>
+);
 
 export default function Members() {
-  const members = [
-    {
-      name: "Maksym Pryshchepa",
-      contacts: "",
-      status: "source community developer",
-      github:{
-        title:"Maks-Mm",
-        link:"https://github.com/Maks-Mm"
-      },
-      portfolioLink:"http://meinportfolio.com",
-    },
-    {
-      name: "Weronika",
-      contacts: "",
-      status: "source community developer",
-      github:{
-        title:"",
-        link:"https://github.com/Putinica",
-      },
-      portfolioLink:"",
-    },
-  ];
-
   return (
     <main>
       <Table variant="dark" striped bordered hover>
@@ -39,18 +71,7 @@ export default function Members() {
           </tr>
         </thead>
         <tbody>
-          {members.map((member, index) => {
-            return (
-              <tr key={index + "table-member"}>
-                <td>{index + 1}</td>
-                <td>{member.name}</td>
-                <td>{}</td>
-                <td>{member.status}</td>
-                <td><a href={member.github.link} target="_blank">{member.github.title}</a></td>
-                <td><a href={member.portfolioLink} target="_blank">Link</a></td>
-              </tr>
-            );
-          })}
+          <MemberRows members={members} />
         </tbody>
       </Table>
     </main>

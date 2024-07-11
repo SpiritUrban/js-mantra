8ec6db94579f7217b665267da2966247d08e2eff
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-07-11 23:37:00 +0300

    fix for dev n prod

--- pages/api/get-pages.ts
+++ pages/api/get-pages.ts
@@ -1,12 +1,32 @@
 // pages/api/get-pages.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const filePath = path.join(process.cwd(), 'pages/api/pages-list.json');
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const pages = JSON.parse(fileContents);
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
-  res.status(200).json(pages);
+  if (isDevelopment) {
+    const directoryPath = path.join(process.cwd(), 'pages/source');
+  
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to read directory' });
+      }
+
+      const pageFiles = files.filter(file => file.endsWith('.js') || file.endsWith('.jsx') || file.endsWith('.ts') || file.endsWith('.tsx'));
+    
+      res.status(200).json({ files: pageFiles });
+    });
+  } else {
+    const filePath = path.join(process.cwd(), 'pages/api/pages-list.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to read pages list' });
+      }
+
+      const pages = JSON.parse(data);
+      res.status(200).json(pages);
+    });
+  }
 }

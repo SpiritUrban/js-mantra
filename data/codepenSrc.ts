23b1477e841565af4ae624aae3cde987108b5ac0
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-07-14 10:44:54 +0300

    add enum

--- data/codepenSrc.ts
+++ data/codepenSrc.ts
@@ -1,6 +1,20 @@
+export enum Category {
+  BUTTON = "button",
+  SLIDER = "slider",
+  PAGE = "page"
+}
+
+export enum Segments {
+  RESULT = "result",
+  JS_RESULT = "js result",
+  CSS_RESULT = "css result",
+  HTML_RESULT = "html result"
+}
+
 export interface Src {
   id: string;
-  segments: string;
+  segments: Segments;
+  category: Category;
 }
 
 export const defaultTab = (item: Src) => item.segments.split(" ").join("%2C");
@@ -8,18 +22,36 @@ export const defaultTab = (item: Src) => item.segments.split(" ").join("%2C");
 export const codepenSrc: Src[] = [
   {
     id: "eYaBKGG",
-    segments: "result",
+    segments: Segments.RESULT,
+    category: Category.BUTTON,
   },
   {
     id: "JjQPQXR",
-    segments: "js result",
+    segments: Segments.JS_RESULT,
+    category: Category.BUTTON,
   },
   {
     id: "JjQPQXR",
-    segments: "css result",
+    segments: Segments.CSS_RESULT,
+    category: Category.SLIDER,
   },
   {
     id: "JjQPQXR",
-    segments: "html result",
+    segments: Segments.HTML_RESULT,
+    category: Category.PAGE,
   },
 ];
+
+
+// Новый объект, который вы хотите добавить
+const newItem: Src = {
+  id: "newId123",
+  segments: Segments.RESULT,
+  category: Category['BUTTON'],
+};
+
+// Добавляем новый объект в массив codepenSrc
+codepenSrc.push(newItem);
+
+// Проверяем содержимое массива
+console.log(codepenSrc);

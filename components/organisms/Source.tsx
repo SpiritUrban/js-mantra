e99a1ff839703f78566c46d96712248ba5dc37a1
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-07-14 11:50:47 +0300

    Source component improvement

--- components/organisms/Source.tsx
+++ components/organisms/Source.tsx
@@ -34,6 +34,27 @@ const Illustration = styled.div<IllustrationProps>`
     }
 `;
 
+const ImageSourceContainer = styled.div`
+    flex: 15;
+     height: 10rem;
+    border-radius: 2rem;
+     margin-top: 1rem;
+    transition: transform 0.5s ease, box-shadow 0.5s ease;
+    transform: perspective(200px) rotateX(20deg) rotateY(20deg);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 6px rgba(0, 0, 0, 0.23);
+    border: black solid 4px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    padding: 1rem;
+    background: darkgray;
+
+    &:hover {
+        transform: perspective(1000px) rotateX(0deg) rotateY(0deg);
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+`;
+
 const Content = styled.div`
     flex: 80;
 `;
@@ -47,14 +68,16 @@ const LinkContainer = styled.div`
 interface SourceProps {
     title: string;
     description: string;
-    imageUrl: string;
+    imageUrl?: string;
+    imageSource?: string;
     sourceLink: string;
 }
 
-const Source: React.FC<SourceProps> = ({ title, description, imageUrl, sourceLink }) => {
+const Source: React.FC<SourceProps> = ({ title, description, imageUrl, imageSource, sourceLink }) => {
     return (
         <Section>
-            <Illustration $backgroundImage={imageUrl} />
+            {imageUrl && (<Illustration $backgroundImage={imageUrl} />)}
+            {imageSource && (<ImageSourceContainer dangerouslySetInnerHTML={{ __html: imageSource }} />)}
             <Content>
                 <h1>{title}</h1>
                 <p>{description}</p>
--- pages/source/example2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const example2 = () => {
-  return (
-    <div>example2😂</div>
-  )
-}
-
-export default example2;

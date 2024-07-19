3aa3c410c18286021d002b44fc5acc0e94c752da
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-07-19 18:42:07 +0300

    add SourceCard

---
+++ components/organisms/SourceCard.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IllustrationProps {
+    $backgroundImage: string;
+}
+
+const Section = styled.section`
+position: relative;
+    height: 15rem;
+    width: 15rem;
+    border-radius: 2rem;
+    transition: transform 0.5s ease, box-shadow 0.5s ease;
+    transform: perspective(350px) rotateX(20deg) rotateY(20deg);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 6px rgba(0, 0, 0, 0.23);
+    border: black solid 4px;
+        box-sizing: content-box;
+
+    &:hover {
+        transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1.5);
+            box-shadow: 0 0 189px rgb(0 0 0), 0 0 124px rgb(0 0 0);
+        z-index: 111;
+    }
+`;
+
+const Illustration = styled.div<IllustrationProps>`
+  position: absolute;
+    left:0;
+    top:0;
+    height: 15rem;
+    width: 15rem;
+    background-image: url(${props => props.$backgroundImage});
+    background-size: cover;
+    background-position: left;
+    border-radius: 2rem;
+
+`;
+
+const ImageSourceContainer = styled.div`
+    position: absolute;
+    left:0;
+    top:0;
+     height: 10rem;
+    border-radius: 2rem;
+     margin-top: 1rem;
+    transition: transform 0.5s ease, box-shadow 0.5s ease;
+    transform: perspective(200px) rotateX(20deg) rotateY(20deg);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 6px rgba(0, 0, 0, 0.23);
+    border: black solid 4px;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    padding: 1rem;
+    background: darkgray;
+
+  
+`;
+
+const Content = styled.div`
+    position: absolute;
+     right:0;
+        bottom: 0rem;
+    background: #000000a6;
+    border-radius: 1rem;
+    padding: .5rem 1rem 0rem 1rem;
+    h1 {
+        font-size: 1.3rem;
+    }
+`;
+
+const LinkContainer = styled.div`
+    position: absolute;
+    display: flex;
+    align-items: center;
+    right: -3rem;
+    top: -1rem;
+    background: #000000a6;
+    border-radius: 1rem;
+    padding: .5rem;
+ 
+`;
+
+interface SourceProps {
+    title: string;
+    description: string;
+    imageUrl?: string;
+    imageSource?: string;
+    sourceLink: string;
+}
+
+const SourceCard: React.FC<SourceProps> = ({ title, description, imageUrl, imageSource, sourceLink }) => {
+    return (
+        <Section>
+            {imageUrl && (<Illustration $backgroundImage={imageUrl} />)}
+            {imageSource && (<ImageSourceContainer dangerouslySetInnerHTML={{ __html: imageSource }} />)}
+            <Content>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </Content>
+            <LinkContainer>
+                <a target="_blank" href={sourceLink || imageUrl || imageSource}>Link</a>
+            </LinkContainer>
+        </Section>
+    );
+};
+
+export default SourceCard;
---
+++ pages/source/game-ui.tsx
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+import Source from '@/components/organisms/Source';
+import SourceCard from '@/components/organisms/SourceCard';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem;
+    max-width: 1200px;
+    margin: 0 auto;
+`;
+
+const Container2 = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    padding: 3rem;
+    max-width: 1200px;
+    margin: 0 auto;
+`;
+
+export default function GameUi() {
+
+    interface Sourcs {
+        title: string;
+        imageUrl?: string;
+        imageSource?: string;
+        sourceLink?: string;
+        description?: string;
+    };
+
+    const data: Sourcs[] = [{
+        title: "Free Game Ui Images",
+        imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        // imageSource: `<img src="https://justpaste.it/static/img/jp_logo_v3.svg" alt="" />`,
+        sourceLink: "https://www.freepik.com/free-photos-vectors/game-ui",
+        description: ""
+    }];
+
+    const data2: Sourcs[] = [
+        {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        },
+        {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        }, {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        },
+        {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        },
+        {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        }, {
+            title: "Free Game Ui Images",
+            imageUrl: "https://img.freepik.com/premium-vector/monster-battle-gui-icon-cartoon-stylized-illustration-with-text-button-game-name_284645-1046.jpg?size=626&ext=jpg",
+        },
+    ];
+
+    return (
+        <div>
+
+
+            <Container>
+                <h1>Game UI</h1>
+
+                {data.map(item => (<Source
+                    key={item.title}
+                    title={item.title}
+                    description={item.description}
+                    imageUrl={item.imageUrl}
+                    imageSource={item.imageSource}
+                    sourceLink={item.sourceLink}
+                />)
+                )}
+
+            </Container>
+
+            <Container2>
+                {data2.map(item => (<SourceCard
+                    key={item.title}
+                    title={item.title}
+                    description={item.description}
+                    imageUrl={item.imageUrl}
+                    imageSource={item.imageSource}
+                    sourceLink={item.sourceLink}
+                />)
+                )}
+            </Container2>
+
+        </div>
+
+    );
+}

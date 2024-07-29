3a98c62047466a4433084b448a9920256473d94d
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-07-29 13:45:31 +0300

    refactor

--- data/training/cum-work.ts
+++ data/training/cum-work.ts
@@ -1,57 +1,55 @@
-import { Training, ModalContent, CumPortion, } from "./interfaces";
+import { Training, ModalContent, CumPortion } from "./interfaces";
 
 const funcName = 'cumMixer';
 
-export default {
+const trainingData: Training = {
+    title: "Cum Work",
+    heading: 'Используем метод <b>"reduce"</b>',
+    funcName,
+    description: `Допишите функцию <b>"${funcName}()"</b>.`,
+    img: "/img/cum-work.webp",
 
-    trainingData: {
-        title: "Cum Work",
-        heading: 'Используем метод <b>"reduce"</b>',
-        funcName,
-        description: `Допишите функцию <b>"${funcName}()"</b>.`,
-        img: "/img/cum-work.webp",
+    test: [
+        {
+            description: `Тест 1: Проверяет, что <b>"${funcName}(data)"</b> возвращает <b>180</b> для набора данных <b>"data"</b> .`,
+            successMessage: `Тест 1 прошел: ${funcName}(data) === 180`,
+            failMessage: `Тест 1 провален: ${funcName}(data) !== 180`,
+            data: [
+                {
+                    producer: "Vasya",
+                    volume: 50 // 50 ml
+                },
+                {
+                    producer: "Sanya",
+                    volume: 60 // 60 ml
+                },
+                {
+                    producer: "Siroja",
+                    volume: 70 // 70 ml
+                }
+            ] as CumPortion[],
+            result: 180
+        },
 
-        test: [
-            {
-                description: `Тест 1: Проверяет, что <b>"${funcName}(data)"</b> возвращает <b>180</b> для набора данных <b>"data"</b> .`,
-                successMessage: `Тест 1 прошел: ${funcName}(data) === 180`,
-                failMessage: `Тест 1 провален: ${funcName}(data) !== 180`,
-                data: [
-                    {
-                        producer: "Vasya",
-                        volume: 50 // 50 ml
-                    },
-                    {
-                        producer: "Sanya",
-                        volume: 60 // 60 ml
-                    },
-                    {
-                        producer: "Siroja",
-                        volume: 70 // 70 ml
-                    }
-                ] as CumPortion[],
-                result: 180
-            },
+        {
+            description: `Тест 2: Проверяет, что <b>"${funcName}([])"</b> возвращает <b>0</b> для пустого массива.`,
+            successMessage: `Тест 2 прошел: ${funcName}([]) === 0`,
+            failMessage: `Тест 2 провален: ${funcName}([]) !== 0`,
+            data: [],
+            result: 0
+        }
+    ],
 
-            {
-                description: `Тест 2: Проверяет, что <b>"${funcName}([])"</b> возвращает <b>0</b> для пустого массива.`,
-                successMessage: `Тест 2 прошел: ${funcName}([]) === 0`,
-                failMessage: `Тест 2 провален: ${funcName}([]) !== 0`,
-                data: [],
-                result: 0
-            }
-        ],
+};
 
-    } as Training,
+const modalContent: ModalContent = {
+    title: 'Congratulation !!!',
+    heading: 'You have passed the test',
+    description: 'You have passed the test and you can get a reward.',
+    img: '/img/medals/cum-worker.webp',
+};
 
-    modalContent: {
-        title: 'Congratulation !!!',
-        heading: 'You have passed the test',
-        description: 'You have passed the test and you can get a reward.',
-        img: '/img/medals/cum-worker.webp',
-    } as ModalContent,
-
-    initialCode: `
+const initialCode: string = `
 interface CumPortion {
     producer: string;
     volume: number;
@@ -74,9 +72,9 @@ const data: CumPortion[] = [
 
 const cumMixer = (cumPortions: CumPortion[]): number => 
     cumPortions.reduce((backet, currentPortion) => /* тут */, 0);
-` as string,
+`;
 
-}
+export default { trainingData, modalContent, initialCode }
 
 // answer -> backet + currentPortion.volume
 
--- data/training/dick-splitter.ts
+++ data/training/dick-splitter.ts
@@ -1,68 +1,55 @@
-import { Training, ModalContent, CumPortion, } from "./interfaces";
+import { Training, ModalContent, CumPortion } from "./interfaces";
 
 const funcName = 'dickSplitter';
 
-export default {
+const trainingData: Training = {
+    title: "Dick Splitting",
+    heading: 'Используем метод <b>"split"</b>',
+    funcName,
+    description: `Допишите функцию <b>"${funcName}()"</b>.`,
+    img: "/img/dick-splitter.webp",
 
-    trainingData: {
-        title: "Dick Splitting",
-        heading: 'Используем метод <b>"split"</b>',
-        funcName,
-        description: `Допишите функцию <b>"${funcName}()"</b>.`,
-        img: "/img/dick-splitter.webp",
+    test: [
+        {
+            description: `Тест 1: Проверяет, что <b>"${funcName}(data)"</b> возвращает <b>180</b> для набора данных <b>"data"</b> .`,
+            successMessage: `Тест 1 прошел: ${funcName}(data) === 180`,
+            failMessage: `Тест 1 провален: ${funcName}(data) !== 180`,
+            data: [
+                {
+                    producer: "Vasya",
+                    volume: 50 // 50 ml
+                },
+                {
+                    producer: "Sanya",
+                    volume: 60 // 60 ml
+                },
+                {
+                    producer: "Siroja",
+                    volume: 70 // 70 ml
+                }
+            ] as CumPortion[],
+            result: 180
+        },
+        {
+            description: `Тест 2: Проверяет, что <b>"${funcName}([])"</b> возвращает <b>0</b> для пустого массива.`,
+            successMessage: `Тест 2 прошел: ${funcName}([]) === 0`,
+            failMessage: `Тест 2 провален: ${funcName}([]) !== 0`,
+            data: [],
+            result: 0
+        }
+    ],
+};
 
-        test: [
-            {
-                description: `Тест 1: Проверяет, что <b>"${funcName}(data)"</b> возвращает <b>180</b> для набора данных <b>"data"</b> .`,
-                successMessage: `Тест 1 прошел: ${funcName}(data) === 180`,
-                failMessage: `Тест 1 провален: ${funcName}(data) !== 180`,
-                data: [
-                    {
-                        producer: "Vasya",
-                        volume: 50 // 50 ml
-                    },
-                    {
-                        producer: "Sanya",
-                        volume: 60 // 60 ml
-                    },
-                    {
-                        producer: "Siroja",
-                        volume: 70 // 70 ml
-                    }
-                ] as CumPortion[],
-                result: 180
-            },
+const modalContent: ModalContent = {
+    title: 'Congratulation !!!',
+    heading: 'You have passed the test',
+    description: 'You have passed the test and you can get a reward.',
+    img: '/img/medals/glass-dick-splitter.webp',
+};
 
-            {
-                description: `Тест 2: Проверяет, что <b>"${funcName}([])"</b> возвращает <b>0</b> для пустого массива.`,
-                successMessage: `Тест 2 прошел: ${funcName}([]) === 0`,
-                failMessage: `Тест 2 провален: ${funcName}([]) !== 0`,
-                data: [],
-                result: 0
-            }
-        ],
-
-    } as Training,
-
-    modalContent: {
-        title: 'Congratulation !!!',
-        heading: 'You have passed the test',
-        description: 'You have passed the test and you can get a reward.',
-        img: '/img/medals/glass-dick-splitter.webp',
-    } as ModalContent,
-
-    initialCode: `
+const initialCode: string = `
 const glassDick: string = 'my dick is big';
 const dickSplitter = (glassDick: string): string[] => glassDick.split(' ');
-` as string,
+`;
 
-}
-
-// answer -> backet + currentPortion.volume
-
-
-
-
-
-
-
+export default { trainingData, modalContent, initialCode };

4887942f27a445f4da8b25cd790d16426cf66c5a
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-08-03 16:25:16 +0200

    clean up

--- public/training/dick-splitter/code.test.js
+++ public/training/dick-splitter/code.test.js
@@ -1,29 +1,25 @@
+describe("dickSplitter", function () {
+  it("должен разбить строку на массив слов", function () {
+    const glassDick = "my dick is big";
+    const result = dickSplitter(glassDick);
+    expect(result).to.deep.equal(["my", "dick", "is", "big"]);
+  });
 
-describe('dickSplitter', function () {
-    it('должен разбить строку на массив слов', function () {
-        const glassDick = 'my dick is big';
-        const result = dickSplitter(glassDick);
-        expect(result).to.deep.equal(['my', 'dick', 'is', 'big']);
-    });
+  it("должен возвращать пустой массив для пустой строки", function () {
+    const glassDick = "";
+    const result = dickSplitter(glassDick);
+    expect(result).to.deep.equal([]);
+  });
 
-    it('должен возвращать пустой массив для пустой строки', function () {
-        const glassDick = '';
-        const result = dickSplitter(glassDick);
-        console.log(result);
-        expect(result).to.deep.equal([]);
-    });
+  it("должен правильно обрабатывать строку с несколькими пробелами", function () {
+    const glassDick = "my  dick   is    big";
+    const result = dickSplitter(glassDick);
+    expect(result).to.deep.equal(["my", "dick", "is", "big"]);
+  });
 
-    it('должен правильно обрабатывать строку с несколькими пробелами', function () {
-        const glassDick = 'my  dick   is    big';
-        const result = dickSplitter(glassDick);
-        console.log(result);
-
-        expect(result).to.deep.equal(['my', 'dick', 'is', 'big']);
-    });
-
-    it('должен возвращать массив из одного элемента для строки без пробелов', function () {
-        const glassDick = 'bigdick';
-        const result = dickSplitter(glassDick);
-        expect(result).to.deep.equal(['bigdick']);
-    });
+  it("должен возвращать массив из одного элемента для строки без пробелов", function () {
+    const glassDick = "bigdick";
+    const result = dickSplitter(glassDick);
+    expect(result).to.deep.equal(["bigdick"]);
+  });
 });

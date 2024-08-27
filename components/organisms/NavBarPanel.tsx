155ea61beb6924f0794acf24b0ae875952aa9be1
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-08-27 11:15:23 +0200

    add register page

--- components/organisms/NavBarPanel.tsx
+++ components/organisms/NavBarPanel.tsx
@@ -1,13 +1,13 @@
-'use client';
-import React, { useRef, useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Link from 'next/link';
-import A from '@/components/atoms/A';
+"use client";
+import React, { useRef, useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Link from "next/link";
+import A from "@/components/atoms/A";
 
 interface LinkItem {
   name: string;
@@ -30,21 +30,21 @@ function NavBarPanel() {
   const [expanded, setExpanded] = useState(false);
   const [pages, setPages] = useState<string[]>([]);
   const [links, setLinks] = useState<NavLinkItem[]>([
-    { name: 'Home', href: '/' },
-    { name: 'Members', href: '/members' },
-    { name: 'Codepen', href: '/codepen-src' },
-    { name: 'React', href: '/react' },
-    { name: 'NLP', href: '/nlp' },
+    { name: "Home", href: "/" },
+    { name: "Members", href: "/members" },
+    { name: "Codepen", href: "/codepen-src" },
+    { name: "React", href: "/react" },
+    { name: "NLP", href: "/nlp" },
     {
-      groupeName: 'Treasury', links: []
+      groupeName: "Treasury",
+      links: [],
     },
     {
-      groupeName: 'Health', links: [
-        { name: 'Issues', href: '/health/issues' },
-      ]
+      groupeName: "Health",
+      links: [{ name: "Issues", href: "/health/issues" }],
     },
-    { name: 'Training', href: '/training' },
-    { name: 'CheatSheets', href: '/cheatsheets' },
+    { name: "Training", href: "/training" },
+    { name: "CheatSheets", href: "/cheatsheets" },
   ]);
 
   type PageListResponse = {
@@ -52,29 +52,36 @@ function NavBarPanel() {
   };
 
   useEffect(() => {
-
-    const removeExtension = (fileName: string) => fileName.replace(/\.(js|jsx|ts|tsx)$/, '');
+    const removeExtension = (fileName: string) =>
+      fileName.replace(/\.(js|jsx|ts|tsx)$/, "");
     const transformLink = (fileName: string) =>
-      fileName === 'index.tsx' ? '/source' : `/source/${removeExtension(fileName)}`;
+      fileName === "index.tsx"
+        ? "/source"
+        : `/source/${removeExtension(fileName)}`;
 
-    fetch('/api/get-pages')
-      .then(response => response.json())
+    fetch("/api/get-pages")
+      .then((response) => response.json())
       .then((data: PageListResponse) => {
         setPages(data.files);
-        setLinks(prevLinks => prevLinks.map(link => {
-          if (isGroupLinkItem(link) && link.groupeName === 'Treasury') {
-            return {
-              ...link,
-              links: [
-                ...link.links,
-                ...data.files.map(file => ({ name: removeExtension(file), href: transformLink(file) })),
-              ]
-            };
-          }
-          return link;
-        }));
+        setLinks((prevLinks) =>
+          prevLinks.map((link) => {
+            if (isGroupLinkItem(link) && link.groupeName === "Treasury") {
+              return {
+                ...link,
+                links: [
+                  ...link.links,
+                  ...data.files.map((file) => ({
+                    name: removeExtension(file),
+                    href: transformLink(file),
+                  })),
+                ],
+              };
+            }
+            return link;
+          })
+        );
       })
-      .catch(error => console.error('Error fetching pages:', error));
+      .catch((error) => console.error("Error fetching pages:", error));
   }, []);
 
   const handleToggle = () => setExpanded(!expanded);
@@ -83,7 +90,12 @@ function NavBarPanel() {
   const toggleRef = useRef<HTMLButtonElement>(null);
 
   return (
-    <Navbar data-bs-theme="dark" expanded={expanded} expand="lg" className="bg-body-tertiary">
+    <Navbar
+      data-bs-theme="dark"
+      expanded={expanded}
+      expand="lg"
+      className="bg-body-tertiary"
+    >
       <Container fluid>
         <Navbar.Brand href="https://js-mantra.vercel.app/">
           <img
@@ -91,21 +103,30 @@ function NavBarPanel() {
             alt="logo"
             style={{
               filter: "invert(100%)",
-              width: '2rem',
-              marginRight: '.7rem'
-            }} />
+              width: "2rem",
+              marginRight: ".7rem",
+            }}
+          />
           js-mantra
         </Navbar.Brand>
-        <Navbar.Toggle ref={toggleRef} aria-controls="navbarScroll" onClick={handleToggle} />
+        <Navbar.Toggle
+          ref={toggleRef}
+          aria-controls="navbarScroll"
+          onClick={handleToggle}
+        />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            {links.map((link, index) => (
+            {links.map((link, index) =>
               isGroupLinkItem(link) ? (
-                <NavDropdown key={index} title={link.groupeName} id={`navbarScrollingDropdown${index}`}>
+                <NavDropdown
+                  key={index}
+                  title={link.groupeName}
+                  id={`navbarScrollingDropdown${index}`}
+                >
                   {link.links.map((sublink, subindex) => (
                     <NavDropdown.Item key={subindex} as="div">
                       <Link href={sublink.href!} passHref legacyBehavior>
@@ -124,11 +145,10 @@ function NavBarPanel() {
                   </Nav.Link>
                 )
               )
-            ))}
+            )}
           </Nav>
-
           {/* SEARCH !!! */}
-          {/* <Form className="d-flex">
+         {/* <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder="Search"
@@ -136,7 +156,14 @@ function NavBarPanel() {
               aria-label="Search"
             />
             <Button variant="outline-success">Search</Button>
-          </Form> */}
+          </Form>*/}
+          <Link href="/auth/register" passHref>
+            Register
+          </Link>
+          |
+          <Link href="/auth/login" passHref>
+            Login
+          </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>

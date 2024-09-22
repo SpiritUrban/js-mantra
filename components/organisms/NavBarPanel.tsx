64a0eda80db4ab2342e71730e9e2516fba0cd085
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-09-22 20:21:34 +0300

    fix

--- components/organisms/NavBarPanel.tsx
+++ components/organisms/NavBarPanel.tsx
@@ -8,6 +8,8 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Link from "next/link";
 import A from "@/components/atoms/A";
+import Image from 'next/image';
+
 
 interface LinkItem {
   name: string;
@@ -98,7 +100,8 @@ function NavBarPanel() {
     >
       <Container fluid>
         <Navbar.Brand href="https://js-mantra.vercel.app/">
-          <img
+          <Image
+            width={500} height={500}
             src="/logo.svg"
             alt="logo"
             style={{
@@ -148,7 +151,7 @@ function NavBarPanel() {
             )}
           </Nav>
           {/* SEARCH !!! */}
-         {/* <Form className="d-flex">
+          {/* <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder="Search"

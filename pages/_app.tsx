92e9d44088c7c94a29dc3b89dc7777087e629666
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-12-19 01:04:20 +0200

    fix layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import { AppProps } from "next/app";
 import { wrapper } from "@/lib/RTK/store";
 import { Provider } from "react-redux";
 import "@/styles/globals.css";
+import Layout from '@/components/layouts/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
 
   return (
     <Provider store={store}>
-      <Component {...props} />
+      <Layout>
+        <Component {...props} />
+      </Layout>
     </Provider>
   );
 }
